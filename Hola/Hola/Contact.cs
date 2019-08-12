df37b67f7a5f2afd914e83da3d5f7a4e6b41183b
Yura Medvedev <suprotive.il@gmail.com>
2019-08-12 17:26:15 +0300

    Added connect and disconnect functionality

---
+++ Hola/Hola/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace Hola
+{
+	public class Contact
+	{
+		public Contact(string remotePort, string localPort, string remoteAddress)
+		{
+			RemotePort = Int32.Parse(remotePort);
+			LocalPort = Int32.Parse(localPort);
+			RemoteAddress = IPAddress.Parse(remoteAddress);
+		}
+
+		protected internal int RemotePort { get; private set; }
+		protected internal int LocalPort { get; private set; }
+		protected internal IPAddress RemoteAddress { get; private set; }
+	}
+}
--- Hola/Hola/MainWindow.xaml.cs
+++ Hola/Hola/MainWindow.xaml.cs
@@ -1,28 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Hola
 {
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
+		static bool isConnected = false;
+		static bool receptionIsWorked = false;
+		static Contact currentContact;
+		static string userName;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			ResetInputData();
+		}
+
+		private void ConnectUser()
+		{
+			try
+			{
+				currentContact = new Contact(tbRemotePort.Text, tbLocalPort.Text, tbRemoteAddress.Text);
+				if (tbUserName.Text == string.Empty)
+					throw new ArgumentNullException();
+				userName = tbUserName.Text;
+
+				btnConDiscon.Content = "Disconnect";
+				isConnected = true;
+				btnSend.IsEnabled = true;
+				tbMessage.IsEnabled = true;
+				btnAddContact.IsEnabled = true;
+				lbChat.Items.Add($"Hi {userName}! You joined to chat!");
+				lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+				//Send message about new connect
+
+				//Run receive task
+			}
+			catch (ArgumentNullException)
+			{
+				ResetInputData();
+				lbChat.Items.Add("Fields 'your name' and 'ip for send' must be filled!");
+				lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+			}
+			catch (FormatException)
+			{
+				ResetInputData();
+				lbChat.Items.Add("Fields 'port for send' and 'port for receive' must be filled with numbers!");
+				lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+			}
+			catch (Exception ex)
+			{
+				ResetInputData();
+				lbChat.Items.Add(ex.Message);
+				lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+			}
+		}
+
+		private void ResetInputData()
+		{
+			currentContact = null;
+			tbRemotePort.Text = "Enter port";
+			tbLocalPort.Text = "Enter port";
+			string[] comandLineArgs = Environment.GetCommandLineArgs();
+			if (comandLineArgs.Length != 1)
+				tbRemoteAddress.Text = comandLineArgs[1];
+			userName = String.Empty;
+			tbUserName.Text = "Enter name";
+
+		}
+
+		private void DisconnectUser()
+		{
+			try
+			{
+				//Send message about disconnect
+				ResetInputData();
+				btnConDiscon.Content = "Connect";
+				isConnected = false;
+				btnSend.IsEnabled = false;
+				tbMessage.IsEnabled = false;
+				btnAddContact.IsEnabled = false;
+				receptionIsWorked = false;
+				lbChat.Items.Add("Diconnected!");
+				lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+			}
+			catch (Exception ex)
+			{
+				lbChat.Items.Add(ex.Message);
+			}
+		}
+
+		private void btnConDiscon_Click(object sender, RoutedEventArgs e)
+		{
+			if (!isConnected)
+				ConnectUser();
+			else
+				DisconnectUser();
 		}
 	}
 }
